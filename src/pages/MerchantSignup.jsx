836f4d9332f5e294773e7070864ca82784d0bb16
Richy <95641769+richardsochima@users.noreply.github.com>
2025-03-01 05:23:16 +0100

    added text to bottom of merchant signup

--- src/pages/MerchantSignup.jsx
+++ src/pages/MerchantSignup.jsx
@@ -229,6 +229,18 @@ export default function MerchantSignup() {
           </Button>
 
           <SocialAuthButtons />
+          <div className="text-center text-sm">
+            By clicking "Create an Account" above, you acknowledge that you have
+            read, understood, and agree to Party Currency's{" "}
+            <a href="/terms" className="text-gold hover:underline">
+              Terms of Service
+            </a>{" "}
+            and{" "}
+            <a href="/privacy" className="text-gold hover:underline">
+              Privacy Policy
+            </a>
+            .
+          </div>
         </form>
       </Form>
     </AuthFormWrapper>
